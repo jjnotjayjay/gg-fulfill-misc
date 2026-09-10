4649089a723d7541b0e36a498ffcc5f02ea6e5aa
ajay-bws <ajay@bassettewebsolutions.com>
2026-09-10 22:28:06 +0530

    Moved other recipients except Neal to CC

--- zapier/mobi-daily-processed-orders-report.js
+++ zapier/mobi-daily-processed-orders-report.js
@@ -9,9 +9,9 @@ const EMAIL_TO_RECIPIENTS = [
   'neal@mobicreations.com',
 ];
 const EMAIL_CC_RECIPIENTS = [
-  'nick@ggfulfill.com',
-  'dennis@ggfulfill.com',
   'team+ggfulfill@bassettewebsolutions.com',
+  'dennis@ggfulfill.com',
+  'nick@ggfulfill.com',
 ];
 
 const CSV_HEADERS = [
